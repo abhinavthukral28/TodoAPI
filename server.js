2dafd34afeb51e0de9c8bb5b1a0f647e0a0e359d
Abhinav Thukral <abhinav@abhinavthukral.com>
2016-08-01 16:19:12 +0530

    RESt done

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore')
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-},{
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}
-];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 app.get('/', function (req, res){
     res.send('Todo Rest API');
 });
@@ -19,18 +15,69 @@ app.get('/todos', function (req, res){
 });
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id);
-    var matchedTodo;
-    for(var i=0;i<todos.length;i++){
-        if(todos[i].id === todoId){
-            matchedTodo = todos[i];
-            break;
-        }
-    }
+    var matchedTodo = _.findWhere(todos, {id: todoId});
     if(matchedTodo){
         res.json(matchedTodo);
     }
     else {
         res.status(404).send();
+    }
+});
+app.post('/todos', function (req, res){
+    var body = _.pick(req.body, 'description', 'completed');
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
+
+    body.id = todoNextId++;
+    todos.push(body);
+    console.log('description');
+    res.json(body);
+
+
+
+});
+app.delete('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    if(matchedTodo){
+        console.log(todos);
+        todos = _.without(todos, matchedTodo);
+        console.log(todos);
+        res.json(matchedTodo);
+    }
+    else {
+        res.status(404).json({"error": "No todos with given id found"});
+    }
+});
+
+app.put('/todos/:id', function(req, res){
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttibutes = {};
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttibutes.completed = body.completed;
+
+    }
+    else if(body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    }
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        validAttibutes.description = body.description;
+
+    }
+    else if(body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    if(matchedTodo){
+        _.extend(matchedTodo, validAttibutes);
+        res.json(matchedTodo);
+    }
+    else {
+        return res.status(404).json({"error": "No todos with given id found"});
     }
 });
 app.listen(PORT, function(){
